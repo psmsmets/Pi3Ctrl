d45c4dbe3c194d383d031e747362e2bf203b5cad
Pieter Smets <mail@pietersmets.be>
2024-08-17 21:52:20 +0200

    test post-install script

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
-import setuptools
+from setuptools import setup
+from setuptools.command.install import install
+from subprocess import check_call
+import atexit
+
+
+def _post_install():
+    """Post-installation."""
+    print('POST INSTALL')
+    # check_call("apt-get install this-package".split())
+
+
+class new_install(install):
+    def __init__(self, *args, **kwargs):
+        super(new_install, self).__init__(*args, **kwargs)
+        atexit.register(_post_install)
+
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setup(cmdclass={'install': new_install})
