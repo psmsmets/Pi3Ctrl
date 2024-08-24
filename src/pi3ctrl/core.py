010f998d144fefe0e8385d8e7130edb7a0bb4d67
Pieter Smets <mail@pietersmets.be>
2024-08-24 23:06:44 +0200

    no need for json

--- src/pi3ctrl/core.py
+++ src/pi3ctrl/core.py
@@ -2,7 +2,6 @@
 from gpiozero import Button, LED
 from threading import Thread
 from time import sleep
-import json
 import os
 import signal
 import subprocess
@@ -22,8 +21,8 @@ config = get_config()
 
 
 # Create button and LED objects
-buttons = [Button(pin) for pin in json.loads(config['BUTTON_PINS'])]
-leds = [LED(pin) for pin in json.loads(config['LED_PINS'])]
+buttons = [Button(pin) for pin in config['BUTTON_PINS']]
+leds = [LED(pin) for pin in config['LED_PINS']]
 
 
 # Function to set LEDs to standby mode
