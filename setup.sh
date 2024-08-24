258490f14eb8bf8141482ac998c732e2dc6cdb7b
Pieter Smets <mail@pietersmets.be>
2024-08-24 21:15:11 +0200

    setup

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+pip install .
+
+sudo rsync -amtv --chown=root:root src/etc/* /etc
+
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
+
+sudo systemctl reset-failed dnsmasq
+sudo systemctl unmask dnsmasq
+sudo systemctl disable dnsmasq
+
+sudo systemctl reset-failed hostapd
+sudo systemctl unmask hostapd
+sudo systemctl disable hostapd
+
+sudo systemctl enable nginx
+sudo systemctl stop nginx
+sudo rm -f /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo systemctl restart nginx
+
+sudo systemctl enable pi3ctrl-core.service 
+sudo systemctl restart pi3ctrl-core.service
+
+sudo systemctl enable pi3ctrl-http.service 
+sudo systemctl restart pi3ctrl-http.service
+
+sudo systemctl enable pi3ctrl-wifi.service 
+sudo systemctl restart pi3ctrl-wifi.service
