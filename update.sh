09f6e694739feaee7922f79a9d3c7861ee45c821
Pieter Smets <mail@pietersmets.be>
2024-08-25 15:20:26 +0200

    update script

--- update.sh
+++ update.sh
@@ -2,9 +2,14 @@
 
 pip install .
 
+# reset systemd
 sudo systemctl daemon-reload
 sudo systemctl reset-failed
 
+# restart pi3ctrl services
 sudo systemctl restart pi3ctrl-core.service
 sudo systemctl restart pi3ctrl-http.service
 sudo systemctl start pi3ctrl-wifi.service
+
+# done
+exit 0
