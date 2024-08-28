0139473041c7806502501824274483a222a1bb76
Pieter Smets <mail@pietersmets.be>
2024-08-28 23:09:11 +0200

    services

--- src/pi3ctrl/utils.py
+++ src/pi3ctrl/utils.py
@@ -16,9 +16,7 @@ core_services = ['pi3ctrl-core.service',
                  'pi3ctrl-http.service',
                  'pi3ctrl-wifi.service',
                  'nginx.service',
-                 'dnsmasq.service',
-                 'hostapd.service',
-                 'wpa_supplicant.service']
+                 'dnsmasq.service']
 
 
 def is_raspberry_pi() -> bool:
